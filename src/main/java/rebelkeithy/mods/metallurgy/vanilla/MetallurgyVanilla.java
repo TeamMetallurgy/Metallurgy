b56665a94582a3c7ea44b99107bac45aaae1b035
Glassmaker <JeanGlassmaker@mail.com>
2014-07-03 10:23:37 +0300

    Changed submods loading order, fixes vanilla set not accessable from the API

--- src/main/java/rebelkeithy/mods/metallurgy/vanilla/MetallurgyVanilla.java
+++ src/main/java/rebelkeithy/mods/metallurgy/vanilla/MetallurgyVanilla.java
@@ -20,7 +20,7 @@ import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
 
-@Mod(modid = "Metallurgy3Vanilla", name = "Metallurgy 3 Vanilla", version = "3.4.0", dependencies = "required-after:Metallurgy3Core")
+@Mod(modid = "Metallurgy3Vanilla", name = "Metallurgy 3 Vanilla", version = "3.4.0", dependencies = "required-after:Metallurgy3Core;after:Metallurgy3Base")
 @NetworkMod(channels = { "MetallurgyVanilla" }, clientSideRequired = true, serverSideRequired = false)
 public class MetallurgyVanilla
 {
